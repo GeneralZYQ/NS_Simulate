29dec08146328e7a40d08e8169711708509ff58a
Yuanqing <zhangyuanqing.generalzyq@gmail.com>
2023-01-29 15:15:26 +0100

    basic scaffolding

---
+++ NS_Simulate/NS_Simulate/Departure/DepartureView.swift
@@ -0,0 +1,20 @@
+//
+//  DepartureView.swift
+//  NS_Simulate
+//
+//  Created by yuanqing zhang on 29/01/2023.
+//
+
+import SwiftUI
+
+struct DepartureView: View {
+    var body: some View {
+        Text("Departure!")
+    }
+}
+
+struct DepartureView_Previews: PreviewProvider {
+    static var previews: some View {
+        DepartureView()
+    }
+}
---
+++ NS_Simulate/NS_Simulate/Plan/PlanView.swift
@@ -0,0 +1,20 @@
+//
+//  PlanView.swift
+//  NS_Simulate
+//
+//  Created by yuanqing zhang on 29/01/2023.
+//
+
+import SwiftUI
+
+struct PlanView: View {
+    var body: some View {
+        Text("Plan!")
+    }
+}
+
+struct PlanView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanView()
+    }
+}
